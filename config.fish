d4712b34eaf9f074d0dc211a855263a87c9c2dcf
Connor Davis <davisc@ainfosec.com>
2018-01-12 22:48:22 -0700

    Add config.fish

---
+++ config.fish
@@ -0,0 +1,16 @@
+#!/usr/bin/fish
+
+#
+# Options for bobthefish
+#
+#set -g theme_date_format "+%a %m.%d.%y %H:%M"
+#set -g theme_nerd_fonts yes
+#set -g theme_display_git_untracked no
+#set -g theme_display_git_ahead_verbose no
+#set -g theme_display_virtualenv no
+#set -g theme_display_ruby no
+#set -g theme_display_vi yes
+#set -g theme_show_exit_status no
+
+set -U fish_color_cwd white
+set -U Z_CMD "j"
