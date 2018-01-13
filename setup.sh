4e8c9b7ca71c7719854860e19ce9bf3b67beda0b
Connor Davis <davisc@ainfosec.com>
2018-01-12 23:07:03 -0700

    Add gitconfig and config.fish

--- setup.sh
+++ setup.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-if [ $# -ne 2 ];
-then
-    echo "Please supply git user.email and git user.name as \$1 and \$2"
-    exit
-fi
-
 dir="$HOME/dotfiles"
 
-ln -sfv $dir/config/fish ~/.config/fish
-ln -sfv $dir/vimrc ~/.vimrc
-ln -sfv $dir/vim ~/.vim
 ln -sfv $dir/bashrc ~/.bashrc
+ln -sfv $dir/config/fish ~/.config/fish
+ln -sfv $dir/gitconfig ~/.gitconfig
+ln -sfv $dir/vim ~/.vim
+ln -sfv $dir/vimrc ~/.vimrc
 
 sudo pacman -Syu --noconfirm
 sudo pacman -S vim git ack fish --needed
@@ -21,13 +16,3 @@ git clone https://github.com/VundleVim/Vundle.vim.git bundle/Vundle.vim
 popd
 
 vim +PluginInstall +qall
-
-git config --global user.email "\"$1\""
-git config --global user.name "\"$2\""
-git config --global core.editor vim
-
-git config --global alias.co 'checkout'
-git config --global alias.br 'branch'
-git config --global alias.ci 'commit'
-git config --global alias.st 'status'
-git config --global alias.re 'remote'
