6f93bb866db046daad60e48853acc7b85879f883
Connor Davis <davisc@ainfosec.com>
2018-01-14 08:25:56 -0700

    Silence ssh-add output

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -17,7 +17,7 @@ end
 function test_identities
     ssh-add -l | grep "The agent has no identities" > /dev/null
     if [ $status -eq 0 ]
-        ssh-add
+        ssh-add > /dev/null
         if [ $status -eq 2 ]
             start_agent
         end
