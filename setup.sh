6683929a7f59d9dcefa5f7e3c26a21d277fad1af
Connor Davis <davisc@ainfosec.com>
2018-01-15 14:24:19 -0700

    Remove target files/dirs before setup

--- setup.sh
+++ setup.sh
@@ -2,16 +2,21 @@
 
 dir="$HOME/dotfiles"
 
-ln -sfv $dir/config/fish ~/.config/fish
-ln -sfv $dir/gitconfig ~/.gitconfig
-ln -sfv $dir/vim ~/.vim
-ln -sfv $dir/vimrc ~/.vimrc
+rm -rf $HOME/.config/fish
+rm -rf $HOME/.gitconfig
+rm -rf $HOME/.vim
+rm -rf $HOME/.vimrc
+
+ln -sfv $dir/config/fish $HOME/.config/fish
+ln -sfv $dir/gitconfig $HOME/.gitconfig
+ln -sfv $dir/vim $HOME/.vim
+ln -sfv $dir/vimrc $HOME/.vimrc
 
 sudo pacman -Syu --noconfirm
 sudo pacman -S python ctags fish git openssh vim --needed
 sudo pacman -S the_silver_searcher --needed
 
-pushd ~/.vim
+pushd $HOME/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git bundle/Vundle.vim
 vim +PluginInstall +qall
 
