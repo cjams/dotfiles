0380ec856b9b70425bf2f396674c3b1160309249
Connor Davis <davisc@ainfosec.com>
2018-01-19 21:37:14 -0700

    Fix EDITOR and add systemctl abbrev

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,6 @@
 #!/usr/bin/fish
 
 set -U fish_color_cwd white
-set -U EDITOR /usr/bin/fish
+
+set -e EDITOR
+set -U EDITOR /usr/bin/vim
---
+++ config/fish/functions/sctl.fish
@@ -0,0 +1,3 @@
+function sctl
+    sudo systemctl $argv
+end
