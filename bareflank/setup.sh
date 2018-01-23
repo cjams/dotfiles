e7900b87d10ce7ba672610bbb83505eccfe3cfb1
Connor Davis <davisc@ainfosec.com>
2018-01-22 18:30:07 -0700

    Delete unneeded remotes

--- bareflank/setup.sh
+++ bareflank/setup.sh
@@ -10,9 +10,6 @@ git clone -b dev git@github.com:connojd/hypervisor.git
 pushd hypervisor
 
 git remote -v add upstream https://github.com/bareflank/hypervisor.git
-git remote -v add zepf https://github.com/jwzepf/hypervisor.git
-git remote -v add wright https://github.com/jaredwright/hypervisor.git
-git remote -v add quinn https://github.com/rianquinn/hypervisor.git
 
 popd
 popd
