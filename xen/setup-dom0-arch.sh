e38fec85f132cd3b9d36d9452392853d34da2cd5
Connor Davis <davisc@ainfosec.com>
2018-07-11 10:12:56 -0600

    Enable xen dom0 services

--- xen/setup-dom0-arch.sh
+++ xen/setup-dom0-arch.sh
@@ -10,5 +10,10 @@ then
     mkdir /boot/efi/EFI/xen
 fi
 
+sudo systemctl enable xenstored
+sudo systemctl enable xenconsoled
+sudo systemctl enable xendomains
+sudo systemctl enable xen-init-dom0
+
 sudo cp xen/xen.efi /boot/efi/EFI/xen/
 echo "Please cp vmlinuz-linux's, initramfs, and xen.cfg to /boot/efi/EFI/xen"
