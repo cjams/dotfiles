fcbe5f4faf1992afd660c4790d88ff60cce17081
Connor Davis <davisc@ainfosec.com>
2018-08-03 15:18:39 -0600

    Update xen script

--- xen/setup-dom0-arch.sh
+++ xen/setup-dom0-arch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo ln -sf /usr/bin/python2 /usr/bin/python
-sudo pacman -S iasl python2-yaml ovmf --needed --no-confirm
-./configure --disable-rombios --disable-docs
+#sudo ln -sf /usr/bin/python2 /usr/bin/python
+#sudo pacman -S iasl python2-yaml ovmf --needed --noconfirm
+./configure --disable-rombios --disable-docs --disable-flask --disable-vtpm-stubdom --disable-vtpmmgr-stubdom --disable-ioemu-stubdom --disable-seabios --prefix=/usr
 sudo make install -j$(nproc)
 
 if [ ! -d /boot/efi/EFI/xen ];
@@ -16,4 +16,5 @@ sudo systemctl enable xendomains
 sudo systemctl enable xen-init-dom0
 
 sudo cp xen/xen.efi /boot/efi/EFI/xen/
+sudo chown -R dev:users .
 echo "Please cp vmlinuz-linux's, initramfs, and xen.cfg to /boot/efi/EFI/xen"
