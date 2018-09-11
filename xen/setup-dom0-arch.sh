64e0b8bcece7108b16aa5322d004ce14eaf9a4b1
Connor Davis <davisc@ainfosec.com>
2018-09-11 16:17:28 -0600

    Update aliases and xen script

--- xen/setup-dom0-arch.sh
+++ xen/setup-dom0-arch.sh
@@ -3,17 +3,18 @@
 sudo ln -sf /usr/bin/python2 /usr/bin/python
 sudo pacman -S yajl bin86 iasl python2-yaml --needed --noconfirm
 ./configure --disable-rombios --disable-docs --disable-stubdom --disable-seabios --prefix=/usr
-make -j$(nproc)
-
-if [ ! -d /boot/efi/EFI/xen ];
-then
-    mkdir /boot/efi/EFI/xen
-fi
+make -j$(nproc) dist-xen
+make -j$(nproc) dist-tools
 
 sudo systemctl enable xenstored
 sudo systemctl enable xenconsoled
 sudo systemctl enable xendomains
 sudo systemctl enable xen-init-dom0
+
+if [ ! -d /boot/efi/EFI/xen ];
+then
+    mkdir /boot/efi/EFI/xen
+fi
 
 sudo cp xen/xen.efi /boot/efi/EFI/xen/
 sudo chown -R dev:users .
