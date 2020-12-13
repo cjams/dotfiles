cbebc5b52d8e5fdbfb61555f4b628b139907602d
Connor Davis <davisc@ainfosec.com>
2020-12-13 12:48:31 -0700

    Use powerlevel10k theme with patched fonts

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
 
-sudo apt install tree vim zsh
-
-pkgs="tree vim zsh silversearcher-ag"
-
+pkgs="tree vim zsh silversearcher-ag curl deepin-terminal"
 sudo apt install -y $pkgs
 
 rm -rf $HOME/.gitconfig
@@ -20,6 +17,9 @@ ln -fsv $dir/vim $HOME/.vim
 ln -fsv $dir/vimrc $HOME/.vimrc
 ln -fsv $dir/bashrc $HOME/.bashrc
 ln -fsv $dir/inputrc $HOME/.inputrc
+ln -fsv $dir/zshrc $HOME/.zshrc
+
+sed -i "s|/home/connojd|/home/$USER|" $HOME/.zshrc
 
 cd $HOME/.vim
 if [ ! -d bundle ];
@@ -28,3 +28,30 @@ then
 fi
 
 vim +PluginInstall +qall
+
+# Get patched powerline fonts
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+
+sudo mkdir -p /usr/share/fonts/truetype
+
+sudo mv 'MesloLGS NF Regular.ttf' /usr/share/fonts/truetype/
+sudo mv 'MesloLGS NF Bold.ttf' /usr/share/fonts/truetype/
+sudo mv 'MesloLGS NF Italic.ttf' /usr/share/fonts/truetype/
+sudo mv 'MesloLGS NF Bold Italic.ttf' /usr/share/fonts/truetype/
+
+fc-cache -f
+sed -i 's|font=\(.*\)|font=MesloLGS NF|' ~/.config/deepin/deepin-terminal/config.conf
+
+# Get oh-my-zsh and plugins
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+
+git clone --depth=1 https://github.com/softmoth/zsh-vim-mode $ZSH_CUSTOM/plugins/zsh-vim-mode
+git clone --depth=1 https://github.com/romkatv/powerlevel10k $ZSH_CUSTOM/themes/powerlevel10k
+
+# Link custom zsh bits
+ln -fsv $dir/oh-my-zsh/custom/alias.zsh $ZSH_CUSTOM/alias.zsh
