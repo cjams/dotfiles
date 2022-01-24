0756c0b4fe8309049405aa62eb85f26cc610e418
Connor Davis <connojdavis@gmail.com>
2022-01-24 22:17:08 +0000

    Add setup for EC2 ubuntu AMI

---
+++ setup-ec2-ubuntu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+dir="$HOME/dotfiles"
+
+sudo apt install -y zsh
+
+# Setup vimage
+cp -v $dir/vimrc $HOME/.vimrc
+
+if [ ! -d $HOME/.vim/bundle ];
+then
+    mkdir -p $HOME/.vim
+    cd $HOME/.vim
+    git clone https://github.com/VundleVim/Vundle.vim.git bundle/Vundle.vim
+    vim +PluginInstall +qall
+fi
+
+cd $HOME
+
+ZSH_BASE="$HOME/.oh-my-zsh"
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+
+if [ ! -d "$ZSH_BASE" ];
+then
+    # Get oh-my-zsh
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    git clone --depth=1 https://github.com/softmoth/zsh-vim-mode $ZSH_CUSTOM/plugins/zsh-vim-mode
+    git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+    git clone --depth=1 https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k $ZSH_CUSTOM/themes/powerlevel10k
+
+    # Link custom zsh bits
+    ln -fsv $dir/oh-my-zsh/custom/alias.zsh $ZSH_CUSTOM/alias.zsh
+    ln -fsv $dir/oh-my-zsh/custom/bindkey.zsh $ZSH_CUSTOM/bindkey.zsh
+fi
